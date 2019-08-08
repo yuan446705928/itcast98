3c06e2b427426380f670071deb5832937ae91348
yuan <y4467059285@163.com>
2019-08-08 16:24:13 +0800

    第二次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
